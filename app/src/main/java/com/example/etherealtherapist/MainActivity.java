4405178cdcdbc6aa24d4c0532837a2fa6a3ba2c9
Rohan <34743825+rohannair18@users.noreply.github.com>
2022-01-04 22:37:18 +0530

    commit 1

---
+++ app/src/main/java/com/example/etherealtherapist/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.etherealtherapist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    TextView btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.getstarted);
+        btn2=(TextView)findViewById(R.id.ialready);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent K= new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(K);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent J= new Intent(getApplicationContext(),StartActivity.class);
+            }
+        });
+
+    }
+}
