7cfe7c8d09dae20ae726dbb4e357e3e94db931d6
Rohan <34743825+rohannair18@users.noreply.github.com>
2022-01-21 11:15:31 +0530

    commit 1

---
+++ app/src/main/java/com/example/etherealtherapist/ForgotActivity.java
@@ -0,0 +1,96 @@
+package com.example.etherealtherapist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.auth.FirebaseAuth;
+import io.github.muddz.styleabletoast.StyleableToast;
+
+
+public class ForgotActivity extends AppCompatActivity {
+
+    private EditText forgotemail;
+    private Button forgotreset;
+    private ProgressBar forgotprogressbar;
+    private MaterialCardView forgotback;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window.setStatusBarColor(getColor(R.color.dark_grey));
+            }
+            forgotemail = findViewById(R.id.forgotemail);
+            forgotreset = findViewById(R.id.forgotreset);
+            forgotprogressbar = findViewById(R.id.forgotprogressbar);
+            forgotback = findViewById(R.id.forgotback);
+
+            forgotback.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(ForgotActivity.this, Login.class);
+                    startActivity(i);
+                }
+            });
+
+            auth = FirebaseAuth.getInstance();
+            forgotreset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    reset();
+                }
+            });
+        }
+    }
+
+    private void reset() {
+        String email = forgotemail.getText().toString().trim();
+
+        if (email.isEmpty()){
+            forgotemail.setError("Email is required");
+            forgotemail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            forgotemail.setError("Please provide a valid email");
+            forgotemail.requestFocus();
+            return;
+        }
+        forgotprogressbar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    StyleableToast.makeText(ForgotActivity.this, "Check your email to reset your password", Toast.LENGTH_SHORT,R.style.customtoast).show();
+                    forgotprogressbar.setVisibility(View.GONE);
+                }
+                else {
+                    StyleableToast.makeText(ForgotActivity.this, "Try again, Something went wrong", Toast.LENGTH_SHORT, R.style.customtoast).show();
+                    forgotprogressbar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/etherealtherapist/Login.java
+++ app/src/main/java/com/example/etherealtherapist/Login.java
@@ -2,63 +2,136 @@ package com.example.etherealtherapist;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import io.github.muddz.styleabletoast.StyleableToast;
 
-public class Login extends AppCompatActivity {
-    Button btn1;
-    EditText email,password;
-    FirebaseAuth Auth;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    EditText emailsignin, passwordsignin;
+    TextView dontsignin, forgotpassword;
+    FirebaseAuth mAuth;
+    Button submitsignin;
+    ProgressBar progressBar;
+
+    @Override
+    public void onBackPressed() {
+        Intent j = new Intent(Login.this, StartActivity.class);
+        startActivity(j);
+        super.onBackPressed();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_login);
-        btn1=(Button)findViewById(R.id.submitsignin);
-        Auth = FirebaseAuth.getInstance();
-        email = findViewById(R.id.emailsignin);
-        password = findViewById(R.id.passwordsignin);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Intent K= new Intent(getApplicationContext(),StartActivity.class);
-                //startActivity(K);
-                String txt_email= email.getText().toString();
-                String txt_password=password.getText().toString();
-                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password))
-                {
-                    Toast.makeText(Login.this, "All fields are required", Toast.LENGTH_SHORT).show();
-                } else
-                {
-                    Auth.signInWithEmailAndPassword(txt_email, txt_password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful())
-                                    {
-                                        Intent intent = new Intent(Login.this, StartActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
-                                        finish();
-                                    } else
-                                    {
-                                        Toast.makeText(Login.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window.setStatusBarColor(getColor(R.color.dark_grey));
             }
-        });
+        }
+
+        emailsignin = findViewById(R.id.emailsignin);
+        passwordsignin = findViewById(R.id.passwordsignin);
+
+        forgotpassword = findViewById(R.id.forgotpassword);
+        forgotpassword.setOnClickListener(this);
+        progressBar = findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        submitsignin = findViewById(R.id.submitsignin);
+        submitsignin.setOnClickListener(this);
+
+        dontsignin = findViewById(R.id.dontsignin);
+        dontsignin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.submitsignin:
+                submitsignin();
+                break;
+            case R.id.dontsignin:
+                startActivity(new Intent(this, JoinActivity.class));
+                break;
+            case R.id.forgotpassword:
+                startActivity(new Intent(this, ForgotActivity.class));
+                break;
+        }
+
+    }
+
+    private void submitsignin() {
+        String email = emailsignin.getText().toString().trim();
+        String password = passwordsignin.getText().toString().trim();
+
+        if(email.isEmpty()){
+            emailsignin.setError("This field is required");
+            emailsignin.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailsignin.setError("Please provide a valid email");
+            emailsignin.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            passwordsignin.setError("This field is required");
+            passwordsignin.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            passwordsignin.setError("Min password length should be 6 characters");
+            passwordsignin.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            progressBar.setVisibility(View.GONE);
+
+                            if (user.isEmailVerified()) {
+                                StyleableToast.makeText(Login.this, "Let's get started!", R.style.customtoast).show();
+                                Intent intent = new Intent(Login.this, StartActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }else {
+                            StyleableToast.makeText(Login.this, "Failed to sign in! Please check your credentials", R.style.customtoast).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
