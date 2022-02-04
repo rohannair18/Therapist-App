9448013d5762e720a157e7fbce82eb0a7f82bb08
Rohan <34743825+rohannair18@users.noreply.github.com>
2022-02-04 14:13:17 +0530

    commit

--- app/src/main/java/com/example/etherealtherapist/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/etherealtherapist/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.etherealtherapist.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.etherealtherapist.R;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.google.android.material.datepicker.CalendarConstraints;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
+
+    CompactCalendarView compactCalendarView;
+    TextView monthyear;
+    private SimpleDateFormat dateFormatmonth=new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return view;
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+        compactCalendarView=view.findViewById(R.id.compactcalendar_view);
+        monthyear=view.findViewById(R.id.MonthYear);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getContext();
 
+                if (dateClicked.toString().compareTo("Sat Oct 21 00:00:00 AST 2022") == 0) {
+                    Toast.makeText(context, "Teachers' Professional Day", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                monthyear.setText(dateFormatmonth.format(firstDayOfNewMonth));
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/etherealtherapist/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/etherealtherapist/Fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.etherealtherapist.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -9,19 +10,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.viewpager.widget.ViewPager;
 
 
+import com.example.etherealtherapist.Activities.MainActivity;
 import com.example.etherealtherapist.HelperClasses.ViewPagerAdapter;
 import com.example.etherealtherapist.R;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 
 public class ProfileFragment extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
-
+    private ImageView edit;
+    private ImageView logout;
+    private FirebaseUser fUser;
+    private DatabaseReference reference;
+    private String profileId;
+    private TextView profilename;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,8 +42,20 @@ public class ProfileFragment extends Fragment {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        profileId = fUser.getUid();
+        profilename = view.findViewById(R.id.profilename);
         tabLayout = view.findViewById(R.id.tablayout);
         viewPager = view.findViewById(R.id.viewpager);
+        logout = view.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getContext(), MainActivity.class));
+                getActivity().finish();
+            }
+        });
         return view;
 
     }
